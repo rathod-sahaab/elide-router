0ce0d16edf436d1a151976d1a184d4747b1cc088
Abhay Raj Singh <abhayonlyone@gmail.com>
2022-06-28 23:00:27 +0530

    fixed unnecessary member variable

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly configService: ConfigService) {
+	constructor(configService: ConfigService) {
 		const secret = configService.get<string>('JWT_ACCESS_TOKEN_SECRET')
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
