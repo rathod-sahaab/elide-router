b666cb0187a8b332414e69832e3adef0cdfca31b
Abhay Raj Singh <abhayonlyone@gmail.com>
2022-08-14 16:40:37 +0000

    packaged fetch from cache otherwise from db logic in a function

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,9 +6,9 @@ import {
 	NotFoundException,
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { Link } from '@prisma/client'
 import { Cache } from 'cache-manager'
 import { getLinkCacheKey } from './commons/functions/cache-keys'
-import { CachePayloadLink } from './commons/types/cache-payload'
 import { LinkRepository } from './data/repositories/link.repository'
 
 @Injectable()
@@ -19,29 +19,36 @@ export class AppService {
 		@Inject(CACHE_MANAGER) private cacheManager: Cache,
 	) {}
 
+	private async getLinkCacheAside({
+		slug,
+	}: {
+		slug: string
+	}): Promise<{ link: Link; cacheHit: boolean }> {
+		const cachedValue = (await this.cacheManager.get(getLinkCacheKey(slug))) as Link
+		return {
+			link: cachedValue || (await this.linkRepository.linkBySlug({ slug })),
+			cacheHit: !!cachedValue,
+		}
+	}
+
 	// Get the url where we should redirect
 	async visitLink({ slug }: { slug: string }): Promise<string> {
 		if (slug === '') {
 			return this.configService.get('FRONTEND_URL')
 		}
 
-		const cachedValue = (await this.cacheManager.get(getLinkCacheKey(slug))) as CachePayloadLink
-
-		const link = cachedValue || (await this.linkRepository.linkBySlug({ slug }))
+		const { link, cacheHit } = await this.getLinkCacheAside({ slug })
 
 		if (!link) {
 			throw new NotFoundException("Link you're looking for doesn't exist")
 		}
 
-		if (!link.active) {
-			throw new ForbiddenException('Requested link is inactive currently.')
+		if (!cacheHit) {
+			await this.cacheManager.set(getLinkCacheKey(slug), link)
 		}
 
-		if (!cachedValue) {
-			console.log('Cache Miss')
-			await this.cacheManager.set(getLinkCacheKey(slug), link)
-		} else {
-			console.log('Cache Hit', JSON.stringify(cachedValue, null, 3))
+		if (!link.active) {
+			throw new ForbiddenException('Requested link is inactive currently.')
 		}
 
 		// TODO: analytics stuff
